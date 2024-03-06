4cc67396d869de07426aebe2d764620e70d8ff63
Vince-arch <vincekimani006@gmail.com>
2024-03-06 21:26:00 +0300

    adding features to products page

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <div></div>
   )
 }
 
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -5,9 +5,10 @@ import Card from '../components/Card';
 const Products = () => {
   return (
   <div>
-    <div name="header" className="text-black pt-32 bg-pink-200">Categories:
+    <div name='header' className='flex flex-row justify-between h pb-10'>
+    <div name="left" className="text-black pt-32 bg-pink-200">Categories:
     <ul className="">
-      <li>Skirts</li>
+      <li>Skirts</li> 
       <li>Dresses</li>
       <li>Trousers</li>
       <li>Tops</li>
@@ -16,6 +17,10 @@ const Products = () => {
       <li>Sweaters</li>
       <li>Jackets</li>
     </ul>
+    </div>
+    <div name='right' className=''> 
+    <img className='' src="https://images.unsplash.com/photo-1522512115668-c09775d6f424?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJsYWNrJTIwd29tYW4lMjBmYXNoaW9ufGVufDB8fDB8fHww"></img>
+    </div>
     </div>
     <div className='pt-10'><Card/></div>
   </div>
