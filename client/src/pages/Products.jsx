5a1037705adf47d3a6f123de66e8c07202ee6265
Vince-arch <vincekimani006@gmail.com>
2024-03-07 22:31:29 +0300

    image change

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -6,7 +6,7 @@ const Products = () => {
   return (
   <div>
     <div name='header' className='flex flex-row justify-between h pb-10'>
-    <div name="left" className="text-black pt-32 bg-pink-200">Categories:
+    <div name="left" className="text-black pt-32 ">Categories:
     <ul className="">
       <li>Skirts</li> 
       <li>Dresses</li>
@@ -18,8 +18,8 @@ const Products = () => {
       <li>Jackets</li>
     </ul>
     </div>
-    <div name='right' className=''> 
-    <img className='' src="https://images.unsplash.com/photo-1522512115668-c09775d6f424?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJsYWNrJTIwd29tYW4lMjBmYXNoaW9ufGVufDB8fDB8fHww"></img>
+    <div name='right' className='w-3/4 '> 
+    <img className='' src="https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
     </div>
     </div>
     <div className='pt-10'><Card/></div>
