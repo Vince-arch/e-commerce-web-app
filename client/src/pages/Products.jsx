5feb326d935b2994f1cf72094a05d0d1cff1ce37
Vince-arch <vincekimani006@gmail.com>
2024-03-11 22:41:28 +0300

    styling

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -5,9 +5,9 @@ import Card from '../components/Card';
 const Products = () => {
   return (
   <div>
-    <div name='header' className='flex flex-row justify-between h pb-10'>
-    <div name="left" className="text-black pt-32 ">Categories:
-    <ul className="">
+    <div name='header' className='flex flex-row space-x-40 max-h-screen  pt-24'>
+    <div name="left" className="text-black text-5xl pl-5 ">Categories:
+    <ul className="text-xl flex flex-col text-center space-y-2 pt-5">
       <li>Skirts</li> 
       <li>Dresses</li>
       <li>Trousers</li>
@@ -18,11 +18,11 @@ const Products = () => {
       <li>Jackets</li>
     </ul>
     </div>
-    <div name='right' className='w-3/4 '> 
-    <img className='' src="https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+    <div name='right' className='max-h-full'> 
+    <img className='max-h-screen ' src="https://images.unsplash.com/photo-1540221652346-e5dd6b50f3e7?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
     </div>
     </div>
-    <div className='pt-10'><Card/></div>
+    <div className='pt-20'><Card/></div>
   </div>
     )
 }
