541a5c9627323261d56861da511dd4a83775dead
Vince-arch <vincekimani006@gmail.com>
2024-03-24 22:25:36 +0300

    adding media plugin

--- server/sanity.config.js
+++ server/sanity.config.js
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {media, mediaAssetSource} from 'sanity-plugin-media'
+
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,16 @@ export default defineConfig({
   projectId: 'd5rukfw3',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), media()],
+  
+  form: {
+    // Don't use this plugin when selecting files only (but allow all other enabled asset sources)
+    file: {
+      assetSources: previousAssetSources => {
+        return previousAssetSources.filter(assetSource => assetSource !== mediaAssetSource)
+      }
+    }
+  },
 
   schema: {
     types: schemaTypes,
