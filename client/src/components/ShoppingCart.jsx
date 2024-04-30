02822c26e18ce6251c31f89a0e0f2989dd5132a0
Vince-arch <vincekimani006@gmail.com>
2024-04-30 21:22:58 +0300

    new change

--- client/src/components/ShoppingCart.jsx
+++ client/src/components/ShoppingCart.jsx
@@ -10,7 +10,7 @@ const ShoppingCart = () => {
         <div name="icon" className="pr-10 pt-5 relative ">
           <ShoppingCartCheckout />
           <span className="bg-red-500 text-white rounded-full absolute bottom-4 right-7 px-2">
-            {itemQuantity}
+            {0}
           </span>
     </div>
     </div>
