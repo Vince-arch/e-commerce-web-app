e7a2b3477ba2b05755b1de5608d6d6d3c4ef74ad
Vince-arch <vincekimani006@gmail.com>
2024-07-14 19:23:16 +0300

    new changes

--- server/config.js
+++ server/config.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-const config = {
-    port: process.env.PORT || 5050
+export default {
+    port: process.env.PORT || 5050,
+    mongoDBURl: process.env.mongoDBURL
   };
   
-  export default config;
+  
--- server/server.js
+++ server/server.js
@@ -2,14 +2,28 @@
 import express from 'express';
 import cors from "cors";
 import config from "./config.js";
+import mongoose from 'mongoose';
+
 
 //Initializing express app
 const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//connect to MongoDB
+mongoose.connect(config.mongoDBURl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('Database connection error:', err));
 
 app.get('/', (req,res) =>{
     res.send('Hello World')
 });
 
 app.listen(config.port, () => {
-    console.log('server is running')
+    console.log(`server is running on port:${config.port}`)
 })
