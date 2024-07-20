668c1aee16e8a79d3b450e1143c0a98e47a795fb
Vince-arch <vincekimani006@gmail.com>
2024-07-20 21:09:37 +0300

    Adding the product route

---
+++ server/routes/products.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import Product from '../models/Product.js';
+
+const router = express.Router();
+
+//Getting all products
+router.get('/', async(req, res) =>{
+    try{
+        const products = await Product.find();
+        res.json(products);
+    }   catch (err) {
+        res.status(500).json({message: err.message});
+    }
+});
+
+//Getting a single product by ID
+router.get('/:id', async (req, res) => {
+    try{
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({message:'Product not found'})
+        res.json(product);
+    } catch (err) {
+        res.send(500).json({message: err.message})
+    }
+})
+
+export default router;
